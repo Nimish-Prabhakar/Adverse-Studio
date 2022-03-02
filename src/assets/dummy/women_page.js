595e4c9a48b527c7bc13c78598a3de1019911a74
Asfar Sharief <asfar.sharief@eagleview.com>
2022-03-02 17:42:52 +0530

    Made slight changes for integrating with UI

---
+++ src/assets/dummy/women_page.js
@@ -0,0 +1,317 @@
+export let women_listing_response ={
+	"api_response": "success",
+	"api_msg_code": 200,
+	"product_minis": [{
+		"product_id": "ae2a9a30-72c8-4965-8cc2-f56721cca922",
+		"product_catalogue_id": 1,
+		"product_category_id": 1,
+		"product_name": "Pink Kaftan",
+		"cost": 300.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/qRtSwFm/kaftan.jpg"
+		}]
+	}, 
+	{
+		"product_id": "ae2a9a30-72c8-4965-8cc2-f56721cca922",
+		"product_catalogue_id": 1,
+		"product_category_id": 1,
+		"product_name": "Pink Kaftan",
+		"cost": 300.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/qRtSwFm/kaftan.jpg"
+		}]
+	},
+	{
+		"product_id": "ae2a9a30-72c8-4965-8cc2-f56721cca922",
+		"product_catalogue_id": 1,
+		"product_category_id": 1,
+		"product_name": "Pink Kaftan",
+		"cost": 300.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/qRtSwFm/kaftan.jpg"
+		}]
+	},
+	{
+		"product_id": "ae2a9a30-72c8-4965-8cc2-f56721cca922",
+		"product_catalogue_id": 1,
+		"product_category_id": 1,
+		"product_name": "Pink Kaftan",
+		"cost": 300.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/qRtSwFm/kaftan.jpg"
+		}]
+	},
+	{
+		"product_id": "d0cfa45e-39fd-4a2c-bd8c-ff2fb2665c84",
+		"product_catalogue_id": 1,
+		"product_category_id": 2,
+		"product_name": "Blue Jeans",
+		"cost": 400.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/xCrPMSf/bottoms.jpg"
+		}]
+	}, 
+	{
+		"product_id": "d0cfa45e-39fd-4a2c-bd8c-ff2fb2665c84",
+		"product_catalogue_id": 1,
+		"product_category_id": 2,
+		"product_name": "Blue Jeans",
+		"cost": 400.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/xCrPMSf/bottoms.jpg"
+		}]
+	}, 
+	{
+		"product_id": "d0cfa45e-39fd-4a2c-bd8c-ff2fb2665c84",
+		"product_catalogue_id": 1,
+		"product_category_id": 2,
+		"product_name": "Blue Jeans",
+		"cost": 400.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/xCrPMSf/bottoms.jpg"
+		}]
+	}, 
+	{
+		"product_id": "d0cfa45e-39fd-4a2c-bd8c-ff2fb2665c84",
+		"product_catalogue_id": 1,
+		"product_category_id": 2,
+		"product_name": "Blue Jeans",
+		"cost": 400.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/xCrPMSf/bottoms.jpg"
+		}]
+	}, 
+	{
+		"product_id": "9106765f-3503-4b64-8dce-d526fcfa7fa8",
+		"product_catalogue_id": 1,
+		"product_category_id": 3,
+		"product_name": "Cool Top",
+		"cost": 500.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/RysvVVV/topImage.jpg"
+		}]
+	},
+	{
+		"product_id": "9106765f-3503-4b64-8dce-d526fcfa7fa8",
+		"product_catalogue_id": 1,
+		"product_category_id": 3,
+		"product_name": "Cool Top",
+		"cost": 500.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/RysvVVV/topImage.jpg"
+		}]
+	},
+	{
+		"product_id": "9106765f-3503-4b64-8dce-d526fcfa7fa8",
+		"product_catalogue_id": 1,
+		"product_category_id": 3,
+		"product_name": "Cool Top",
+		"cost": 500.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/RysvVVV/topImage.jpg"
+		}]
+	},
+	{
+		"product_id": "9106765f-3503-4b64-8dce-d526fcfa7fa8",
+		"product_catalogue_id": 1,
+		"product_category_id": 3,
+		"product_name": "Cool Top",
+		"cost": 500.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/RysvVVV/topImage.jpg"
+		}]
+	},
+	{
+		"product_id": "2d18bc86-5d02-4e59-9fc4-af9c44cc80f5",
+		"product_catalogue_id": 1,
+		"product_category_id": 4,
+		"product_name": "Flower Dress",
+		"cost": 900.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/hMh9VNh/dress.webp"
+		}]
+	}, 
+	{
+		"product_id": "2d18bc86-5d02-4e59-9fc4-af9c44cc80f5",
+		"product_catalogue_id": 1,
+		"product_category_id": 4,
+		"product_name": "Flower Dress",
+		"cost": 900.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/hMh9VNh/dress.webp"
+		}]
+	}, 
+	{
+		"product_id": "2d18bc86-5d02-4e59-9fc4-af9c44cc80f5",
+		"product_catalogue_id": 1,
+		"product_category_id": 4,
+		"product_name": "Flower Dress",
+		"cost": 900.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/hMh9VNh/dress.webp"
+		}]
+	}, 
+	{
+		"product_id": "2d18bc86-5d02-4e59-9fc4-af9c44cc80f5",
+		"product_catalogue_id": 1,
+		"product_category_id": 4,
+		"product_name": "Flower Dress",
+		"cost": 900.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/hMh9VNh/dress.webp"
+		}]
+	}, 
+	{
+		"product_id": "1440732d-c3c8-47b6-8cae-dc3c80159c7f",
+		"product_catalogue_id": 1,
+		"product_category_id": 5,
+		"product_name": "Nice Crochets",
+		"cost": 200.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/FWk8xSj/crochet.jpg"
+		}]
+	},
+	{
+		"product_id": "1440732d-c3c8-47b6-8cae-dc3c80159c7f",
+		"product_catalogue_id": 1,
+		"product_category_id": 5,
+		"product_name": "Nice Crochets",
+		"cost": 200.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/FWk8xSj/crochet.jpg"
+		}]
+	},
+	{
+		"product_id": "1440732d-c3c8-47b6-8cae-dc3c80159c7f",
+		"product_catalogue_id": 1,
+		"product_category_id": 5,
+		"product_name": "Nice Crochets",
+		"cost": 200.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/FWk8xSj/crochet.jpg"
+		}]
+	},
+	{
+		"product_id": "1440732d-c3c8-47b6-8cae-dc3c80159c7f",
+		"product_catalogue_id": 1,
+		"product_category_id": 5,
+		"product_name": "Nice Crochets",
+		"cost": 200.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/FWk8xSj/crochet.jpg"
+		}]
+	},
+	 {
+		"product_id": "d9807fc9-5246-4030-831c-596ddc1a4f47",
+		"product_catalogue_id": 1,
+		"product_category_id": 6,
+		"product_name": "Cool Sunglassess",
+		"cost": 50.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/3spR9Cc/accessories.jpg"
+		}]
+	},
+	{
+		"product_id": "d9807fc9-5246-4030-831c-596ddc1a4f47",
+		"product_catalogue_id": 1,
+		"product_category_id": 6,
+		"product_name": "Cool Sunglassess",
+		"cost": 50.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/3spR9Cc/accessories.jpg"
+		}]
+	},
+	{
+		"product_id": "d9807fc9-5246-4030-831c-596ddc1a4f47",
+		"product_catalogue_id": 1,
+		"product_category_id": 6,
+		"product_name": "Cool Sunglassess",
+		"cost": 50.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/3spR9Cc/accessories.jpg"
+		}]
+	},
+	{
+		"product_id": "d9807fc9-5246-4030-831c-596ddc1a4f47",
+		"product_catalogue_id": 1,
+		"product_category_id": 6,
+		"product_name": "Cool Sunglassess",
+		"cost": 50.00,
+		"discounted_prize": 0,
+		"discount_percetage": "",
+		"image_urls": [{
+			"prefrence": 1,
+			"image_url": "https://i.ibb.co/3spR9Cc/accessories.jpg"
+		}]
+	}
+]
+}
