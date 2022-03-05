b8b91e73802d12ce91ec37a64f29adaeff38dd4b
Nishu <nimish.prabhakar@eagleview.com>
2022-03-05 15:26:20 +0530

    Added redux code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavigationBar from './Pages/NavigationBar';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import HomePage from './Pages/HomePage';
 import WomenPage from './Pages/WomenPage';
 import SingleProductPage from './Pages/SingleProductPage';
+import { getWomenPageProducts } from './redux/reducers/womenPageReducer';
 
 const theme = createTheme({
   typography: {
@@ -12,6 +15,18 @@ const theme = createTheme({
 });
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWomenPageProducts());
+  }, [dispatch]);
+
+  const womenPageProducts = useSelector(
+    (state) => state.womenPage.womenPageProducts
+  );
+
+  console.log('womenPageProducts', womenPageProducts[0].json);
+
   return (
     <>
       <ThemeProvider theme={theme}>
