76ce9c22f583c4de533370b99ae884163b98ba4d
Nishu <nimish.prabhakar@eagleview.com>
2022-06-24 15:56:47 +0530

    Added iop capture

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavigationBar from './Pages/NavigationBar';
 import HomePage from './Pages/HomePage';
@@ -13,8 +13,27 @@ import OrderSuccess from './Pages/OrderSuccessPage';
 import UserAccount from './Pages/UserAccountPage';
 import PreviousOrdersPage from './Pages/PreviousOrdersPage';
 import ScrollToTop from './utils/ScrollToTop';
+import axios from 'axios';
 
 function App() {
+  const [ip, setIP] = useState('');
+
+  //creating function to load ip address from the API
+  const getData = async () => {
+    const res = await axios.get('https://geolocation-db.com/json/');
+    console.log(res.data);
+    setIP(res.data.IPv4);
+  };
+
+  useEffect(() => {
+    //passing getData method to the lifecycle method
+    getData();
+  }, []);
+
+  useEffect(() => {
+    console.log('ip address is', ip);
+  }, [ip]);
+
   return (
     <>
       <BrowserRouter>
